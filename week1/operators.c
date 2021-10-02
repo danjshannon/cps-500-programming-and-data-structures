92b74a2e1bc13b4176af3bb5e001d3c8c2421057
Daniel Shannon <dan.j.shannon@gmail.com>
2021-10-02 14:08:52 -0600

    operators

--- week1/operators.c
+++ week1/operators.c
@@ -6,16 +6,21 @@ int main(){
     int subtraction;
     int multiplication,division,modulus;
 
+    printf("addition is : %d\n",addition);
+    printf("subtraction is : %d\n",subtraction);
+    printf("multiplication is : %d\n",multiplication);
+    printf("division is : %d\n",division);
+    printf("modulus is : %d\n",modulus);
     addition=a+b;
     subtraction=a-b;
     multiplication=a*b;
     division=a/b;
     modulus=a%b;
 
-    printf("addition of two numbers a,b is : %d\n",addition);
-    printf("subtraction of two numbers a,b is : %d\n",subtraction);
-    printf("multiplication of two numbers a,b is : %d\n",multiplication);
-    printf("division of two numbers a,b is : %d\n",division);
-    printf("modulus of two numbers a,b is : %d\n",modulus);
+    printf("addition of two numbers %d,%d is : %d\n",a,b,addition);
+    printf("subtraction of two numbers %d,%d is : %d\n",a,b,subtraction);
+    printf("multiplication of two numbers %d,%d is : %d\n",a,b,multiplication);
+    printf("division of two numbers %d,%d is : %d\n",a,b,division);
+    printf("modulus of two numbers %d,%d is : %d\n",a,b,modulus);
     return 0;
 }
